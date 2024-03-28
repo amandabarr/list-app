8319446bff16af33e692177231d21d5b7ce8aef6
amandabarr <amandabarryoga@gmail.com>
2024-03-28 09:45:58 -0700

    save tasks to database using indexedDB

--- create-list.js
+++ create-list.js
@@ -1,6 +1,32 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
+const request = window.indexedDB.open("ToDoListDatabase", 1);
+
+let db;
+
+// listens for errors that might occurr while attempting to open the database
+request.onerror = (event) => {
+    console.error(`Database error: ${event.target.errorCode}`);
+};
+
+request.onupgradeneeded = (event) => {
+    const db = event.target.result;
+    const objectStore = db.objectStore("tasks", { keyPath: "id", autoIncrement: true });
+    objectStore.createIndex("task", "task", { unique: false });
+};
+
+// listens for the successful opening of the database
+request.onsuccess = (event) => {
+    console.log("Database opened successfully")
+    // db now holds reference to the opened database
+    db = event.target.result;
+
+    //create renderList function and populate from the database using get/getAll
+
+    //also save done true/false to DB and only populate not done
+};
+
 function addTask() {
     if(inputBox.value === '') {
         alert("You must write something!");
@@ -13,11 +39,25 @@ function addTask() {
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
         li.appendChild(span);
+        addTaskToDB(inputBox.value);
     }
     // now we want to make sure that the text box is empty
     inputBox.value = "";
-    saveData();
-}
+    // saveData();
+};
+
+function addTaskToDB(task) {
+    const transaction = db.transaction("tasks", "readwrite");
+    const objectStore = transaction.objectStore("tasks");
+    const request = objectStore.add({ task: task })
+        request.onsuccess = () => {
+            console.log("Task added to the database");
+        };
+
+        request.onerror = (event) => {
+            console.error(`Error adding task to the database: ${event.target.error}`);
+        };
+};
 
 listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
@@ -29,12 +69,12 @@ listContainer.addEventListener("click", function(e){
     }
 }, false);
 
-function saveData() {
-    localStorage.setItem("data", listContainer.innerHTML);
-}
+// function saveData() {
+//     localStorage.setItem("data", listContainer.innerHTML);
+// }
 
-function showTask(){
-    listContainer.innerHTML = localStorage.getItem("data");
-}
+// function showTask(){
+//     listContainer.innerHTML = localStorage.getItem("data");
+// }
 
-showTask();
+// showTask();
