2a20f7b0bd733f29e049b6446721841b774abddf
amandabarr <amandabarryoga@gmail.com>
2024-07-19 09:52:09 -0700

    updated to using get() and put() methods instead of cursor and only one funtion to add li items

--- create-list.js
+++ create-list.js
@@ -37,6 +37,7 @@ function renderTasks() {
   const transaction = db.transaction("tasks", "readonly");
   const objectStore = transaction.objectStore("tasks");
   const request = objectStore.getAll();
+  listContainer.innerHTML = "";
 
   request.onsuccess = (event) => {
     const tasks = event.target.result;
@@ -44,6 +45,7 @@ function renderTasks() {
       let li = document.createElement("li");
       li.innerHTML = task.task;
       li.setAttribute("id", task.id);
+      console.log(task, task.id);
 
       let span = document.createElement("span");
       span.className = "close";
@@ -71,16 +73,6 @@ function addTask() {
   if (inputBox.value === "") {
     alert("You must write something!");
   } else {
-    let li = document.createElement("li");
-    // what type of element are we creating? "li" - a list item element <li>
-    li.innerHTML = inputBox.value;
-    // now we need to say where this list item shoud be displayed: display in this container
-    listContainer.appendChild(li);
-    let span = document.createElement("span");
-    // add a class name to the span so we can target the x to close
-    span.className = "close";
-    span.innerHTML = "\u00d7";
-    li.appendChild(span);
     addTaskToDB(inputBox.value);
   }
   // now we want to make sure that the text box is empty
@@ -96,11 +88,7 @@ function addTaskToDB(task) {
   const request = objectStore.add({ task: task, done: false, visible: true });
   request.onsuccess = () => {
     console.log("Task: " + task + " added to the database");
-    const task_request = db.transaction("tasks").objectStore("tasks").getAll();
-    task_request.onsuccess = () => {
-      const tasks = task_request.result;
-      console.table(tasks);
-    };
+    renderTasks();
   };
 
   request.onerror = (event) => {
@@ -157,55 +145,17 @@ listContainer.addEventListener(
       const id = Number(e.target.parentElement.getAttribute("id"));
       console.log("Task id: ", id);
       const request = objectStore.get(id);
-      // get method returns an object selevted by a specified key from an object store in the database, in this case the "key"
-      request.onsuccess = () => {
+      // get method returns an object selected by a specified key from an object store in the database, in this case the "key" id
+      request.onsuccess = (event) => {
         const task = request.result;
         // in previous cursor example, I set task to cursor.value(), so task was set to the value of the current cursor
+        // debugger;
         task.visible = false;
+
         objectStore.put(task);
         e.target.parentElement.remove();
       };
-
-      //       const id = e.target.parentElement.getAttribute("id");
-      // const request = objectStore.get(id)
-      // request.onsuccess = function (params) {
-      //       const task = console.log(request.result);
-      //       task.visible = true;
-      //       objectStore.put(task, id);
     }
-
-    // objectStore.openCursor().onsuccess = (event) => {
-    //   const cursor = event.target.result;
-    //   if (cursor) {
-    //     const task = cursor.value;
-    //     const listItem = e.target.parentElement;
-    //     const textContentWithoutX = Array.from(listItem.childNodes)
-    //       .filter((node) => node.nodeName !== "SPAN")
-    //       .map((node) => node.textContent)
-    //       .join("");
-
-    //     if (task.task === textContentWithoutX) {
-    //       task.visible = false;
-    //       e.target.parentElement.remove();
-
-    //       const updateRequest = cursor.update(task);
-    //       updateRequest.onsuccess = () => {
-    //         console.log("This item is now removed");
-    //         cursor.continue();
-    //       };
-
-    //       updateRequest.onerror = (event) => {
-    //         console.error(
-    //           `Error updating task visibility in the database: ${event.event.target.error}`,
-    //         );
-    //       };
-    //     }
-    //   } else {
-    //     cursor.continue();
-    //   }
-    // };
-    // objectStore.delete(task);
-    // saveData();
   },
   false,
 );
